32a2ff1903f207bf79272b2f4f171099e3907fd5
David Pérez Conde <dpconde.me@gmail.com>
2022-11-15 12:25:09 +0100

    first commit

---
+++ src/main/kotlin/com/dpconde/kaicare/surveybot/SurveyBotApplication.kt
@@ -0,0 +1,11 @@
+package com.dpconde.kaicare.surveybot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SurveyBotApplication
+
+fun main(args: Array<String>) {
+	runApplication<SurveyBotApplication>(*args)
+}
---
+++ src/test/kotlin/com/dpconde/kaicare/surveybot/SurveyBotApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dpconde.kaicare.surveybot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SurveyBotApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
